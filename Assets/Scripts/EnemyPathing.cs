e73ad95b2a05f4db3436ff5e8a44c2898dc209f7
steph <superhappyfunsexytime@gmail.com>
2019-07-01 22:12:11 -0400

    Spawn multiple waves of enemies and add damage manager

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -3,13 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyPathing : MonoBehaviour {
-	[SerializeField] List<Transform> waypoints;
-	[SerializeField] float moveSpeed = 2f;
+	WaveConfig waveConfig;
 
+	List<Transform> waypoints;
 	int waypointIndex = 0;
 
 	// Start is called before the first frame update
     void Start() {
+		waypoints = waveConfig.GetWaypoints();
 		transform.position = waypoints[waypointIndex].transform.position;
     }
 
@@ -18,10 +19,14 @@ public class EnemyPathing : MonoBehaviour {
 		Move();
 	}
 
+	public void SetWaveConfig(WaveConfig waveConfig) {
+		this.waveConfig = waveConfig;
+	}
+
 	private void Move() {
 		if (waypointIndex <= waypoints.Count - 1) {
 			var targetPosition = waypoints[waypointIndex].transform.position;
-			var movementThisFrame = moveSpeed * Time.deltaTime;
+			var movementThisFrame = waveConfig.GetMoveSpeed() * Time.deltaTime;
 			transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
 
 			if (transform.position == targetPosition) {
