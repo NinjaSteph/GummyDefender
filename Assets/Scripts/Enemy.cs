f71e005df63cd3acd0750eef82f1adb6e571f06a
steph <superhappyfunsexytime@gmail.com>
2019-09-14 20:02:43 -0400

    Add new enemy for more variation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -8,7 +8,7 @@ public class Enemy : MonoBehaviour {
 	[SerializeField] int scoreValue = 150;
 
 	[Header("Shooting")]
-	[SerializeField] float shotCounter;
+	float shotCounter;
 	[SerializeField] float minTimeBetweenShots = 0.2f;
 	[SerializeField] float maxTimeBetweenShots = 3f;
 	[SerializeField] float projectileSpeed = 10f;
